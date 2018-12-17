c0a875e4f09505c4fb2aff4aeb5a1b8ff757d57d
vidhi1293 <35034821+vidhi1293@users.noreply.github.com>
2018-12-17 17:28:41 +0530

    menu done

--- logs/log-2018-12-17.php
+++ logs/log-2018-12-17.php
@@ -24,3 +24,15 @@ LEFT JOIN `tblmstevaluationtype` `tmet` ON `tmet`.`EvaluationTypeId`=`tmq`.`Eval
 LEFT JOIN `tblmstconfiguration` `tmc` ON `tmc`.`Value`=`tmq`.`AnswerTypeId`
 WHERE `tmc`.`Key` = 'AnswerType'
 ORDER BY `tmq`.`QuestionId`
+ERROR - 2018-12-17 16:51:16 --> Query error: Unknown column 'tmq.EvaluationTypeId' in 'on clause' - Invalid query: SELECT `tmq`.`QuestionId`, COALESCE(tmet.EvaluationTypeName, "All (Common for all Evaluation)") as EvaluationType, `tmq`.`QuestionText`, `tmc`.`DisplayText` as `QuestionType`, `tmq`.`IsActive`, (SELECT COUNT(tea.EvaluationAnswerId) FROM tblevaluationanswer as tea WHERE tmq.QuestionId=tea.QuestionId) as isdisabled
+FROM `tblmstquestion` `tmq`
+LEFT JOIN `tblmstevaluationtype` `tmet` ON `tmet`.`EvaluationTypeId`=`tmq`.`EvaluationTypeId`
+LEFT JOIN `tblmstconfiguration` `tmc` ON `tmc`.`Value`=`tmq`.`AnswerTypeId`
+WHERE `tmc`.`Key` = 'AnswerType'
+ORDER BY `tmq`.`QuestionId`
+ERROR - 2018-12-17 16:55:08 --> Query error: Unknown column 'tmq.EvaluationTypeId' in 'on clause' - Invalid query: SELECT `tmq`.`QuestionId`, COALESCE(tmet.EvaluationTypeName, "All (Common for all Evaluation)") as EvaluationType, `tmq`.`QuestionText`, `tmc`.`DisplayText` as `QuestionType`, `tmq`.`IsActive`, (SELECT COUNT(tea.EvaluationAnswerId) FROM tblevaluationanswer as tea WHERE tmq.QuestionId=tea.QuestionId) as isdisabled
+FROM `tblmstquestion` `tmq`
+LEFT JOIN `tblmstevaluationtype` `tmet` ON `tmet`.`EvaluationTypeId`=`tmq`.`EvaluationTypeId`
+LEFT JOIN `tblmstconfiguration` `tmc` ON `tmc`.`Value`=`tmq`.`AnswerTypeId`
+WHERE `tmc`.`Key` = 'AnswerType'
+ORDER BY `tmq`.`QuestionId`
