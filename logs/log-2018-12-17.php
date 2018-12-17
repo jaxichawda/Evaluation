37c0fdef71a0d569626013369f7100f3e2694600
krupalimistry <37441236+krupalimistry@users.noreply.github.com>
2018-12-17 17:06:32 +0530

    g

---
+++ logs/log-2018-12-17.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-12-17 17:05:18 --> Query error: Column 'EvaluatorId' cannot be null - Invalid query: INSERT INTO `tblmstempevaluator` (`EvaluationId`, `EvaluatorId`, `StatusId`, `IsActive`, `CreatedBy`, `CreatedOn`, `UpdatedBy`, `UpdatedOn`) VALUES (22, NULL, 0, 1, '1', '18-12-17 17:05:18', '1', '18-12-17 17:05:18')
